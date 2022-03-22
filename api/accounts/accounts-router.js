3bd5eb62d5cfeb813aec01079bcd14df47067967
Stefan Miller <stefanmiller@Stefans-MacBook-Pro.local>
2022-03-22 04:41:02 -0400

    final solution

--- api/accounts/accounts-router.js
+++ api/accounts/accounts-router.js
@@ -22,7 +22,8 @@ router.post(
   async (req, res, next) => {
   // DO YOUR MAGIC
   try {
-    const newAccount = await Account.create(req.body)
+    const newAccount = await Account.create({name: req.body.name.trim(),
+    budget: req.body.budget})
     res.status(201).json(newAccount)
 
   } catch (err) {
